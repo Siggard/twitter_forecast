d9782a74d114973d553ed700a675cd5abe80e340
Siggard <Siggard@ya.ru>
2019-07-23 17:21:34 +0500

    - keys

--- start.py
+++ start.py
@@ -4,10 +4,10 @@ print("Hello %s" % str)
 import tweepy
 
 
-consumer_key = '';
-consumer_secret = '';
-access_token = '';
-access_token_secret = '';
+consumer_key = 'gijaNPO1RVGy2lKkCUxUw';
+consumer_secret = 'gWCy6OIpHYGhSMrj0D5pjOmnJQP55M7BF0X35T6XMQ';
+access_token = '363931816-LGB5m13W9oxdj8ktG3eBw1box4W0YBLqI4FP8PkL';
+access_token_secret = 'zgBBsmYczXncka89ONP7LfyeZbdl7cYeE4cRGEh5TeA';
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
