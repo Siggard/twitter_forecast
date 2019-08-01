b25e352513e7399422c5a9b61fd146d1e195dd63
MIKHAIL <smirnov.ma@outlook.com>
2019-08-01 22:12:22 +0500

    in the variable "count_twitts" while manually set the number of downloaded latest tweets

--- start.py
+++ start.py
@@ -2,26 +2,39 @@ import tweepy
 import os
 import glob
 
-
-consumer_key = 'gijaNPO1RVGy2lKkCUxUw';
-consumer_secret = 'gWCy6OIpHYGhSMrj0D5pjOmnJQP55M7BF0X35T6XMQ';
-access_token = '363931816-LGB5m13W9oxdj8ktG3eBw1box4W0YBLqI4FP8PkL';
-access_token_secret = 'zgBBsmYczXncka89ONP7LfyeZbdl7cYeE4cRGEh5TeA';
-path_to_dir = 'clubs';
+consumer_key = 'gijaNPO1RVGy2lKkCUxUw'
+consumer_secret = 'gWCy6OIpHYGhSMrj0D5pjOmnJQP55M7BF0X35T6XMQ'
+access_token = '363931816-LGB5m13W9oxdj8ktG3eBw1box4W0YBLqI4FP8PkL'
+access_token_secret = 'zgBBsmYczXncka89ONP7LfyeZbdl7cYeE4cRGEh5TeA'
+path_to_dir = 'clubs'
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+#in 'count_twitts' until you manually set the number of tweets
+def twitts_user(user_name, count_twitts = 3):
+   api = tweepy.API(auth)
+   user = api.get_user(user_name)
+   screen_name_user = user.screen_name
+   twitter_user_id = user.id 
+   public_tweets = api.user_timeline(user_name, count = count_twitts)
+   for tweet in public_tweets:
+      print(tweet.text)
 
 #specify the directory and search for text files
 os.chdir(path_to_dir) 
 for file in glob.glob("*.txt"):
-	#read lines in file
-	print("\nFile: " + file + "\n")
-	file_open = open(file)
-	for line in file_open:
-		#tweepy API work, connect to account and take feed
-		# TODO: get twitter feed by select account
-		print("String: " + line)
+    #read lines in file
+    #print("\nFile: " + file + "\n")
+    file_open = open(file)
+    for line in file_open:
+        #print('String: ' + line)
+        #trim the link
+        name_long = list(line)
+        for i in range(0,20):
+            del name_long[0]
+        name_short =''.join(name_long)
+        #print(name_short)
+        twitts_user(name_short)
 
 #api = tweepy.API(auth)
 #public_tweets = api.home_timeline()
