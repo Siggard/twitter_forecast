9e5b5eeb9c7e919a58c2758cc596d5052d57bb49
MIKHAIL <smirnov.ma@outlook.com>
2019-08-07 19:25:08 +0500

    Create the database and the first table. Adding to the database the name of the account, the time of the last request tweets (readable string) and since the beginning of the era (for further work with the frequency of requests)

--- start.py
+++ start.py
@@ -1,6 +1,8 @@
 import tweepy
 import os
 import glob
+import sqlite3
+import time
 
 consumer_key = 'gijaNPO1RVGy2lKkCUxUw'
 consumer_secret = 'gWCy6OIpHYGhSMrj0D5pjOmnJQP55M7BF0X35T6XMQ'
@@ -12,26 +14,30 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-def twitts_user(user_name, count_twitts = 1):
-   user = api.get_user(user_name)
-   screen_name_user = user.screen_name
-   twitter_user_id = user.id 
-   public_tweets = api.user_timeline(user_name, count = count_twitts)
-   for tweet in public_tweets:
-      print(tweet.text)
+con = sqlite3.connect('bas_twitts.db')
+cur = con.cursor()
+cur.execute('CREATE TABLE IF NOT EXISTS account_data(Account TEXT, DateLastParse TEXT, DataFromEpoch FLOAT)')
+con.commit()
 
-os.chdir(path_to_dir) 
+
+def twitts_user(user_name, count_twitts=1):
+    user = api.get_user(user_name)
+    public_tweets = api.user_timeline(user_name, count=count_twitts)
+    for tweet in public_tweets:
+        print(tweet.text)
+
+
+os.chdir(path_to_dir)
 for file in glob.glob("*.txt"):
-    name_file = file
-    print("\nFile: " + file + "\n")
+    # print("\nFile: " + file + "\n")
     file_open = open(file)
     for line in file_open:
         name_line = line
         name_short = name_line.split('/')
         name_short = name_short[-1]
+        time_epoch = time.time()
+        full_time_name = time.ctime(time.time())
+        count = [name_short, full_time_name, time_epoch]
+        cur.execute('INSERT INTO account_data VALUES(?, ?, ?)', count)
+        con.commit()
         twitts_user(name_short)
-
-#api = tweepy.API(auth)
-#public_tweets = api.home_timeline()
-#for tweet in public_tweets:
-   #print(tweet.text)
