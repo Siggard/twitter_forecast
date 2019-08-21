262b49faace30a9502f40aab17de08b937d5f8c5
MIKHAIL <smirnov.ma@outlook.com>
2019-08-21 09:09:15 +0500

    Made changes to the creation of databases, added comments and text output to the screen of the script process

--- start.py
+++ start.py
@@ -1,10 +1,11 @@
 import tweepy
 import sqlite3
+import time
+import datetime
 from os import chdir
 from glob import glob
 from pathlib import Path
-from time import sleep, ctime
-
+from re import findall
 
 consumer_key = 'gijaNPO1RVGy2lKkCUxUw'
 consumer_secret = 'gWCy6OIpHYGhSMrj0D5pjOmnJQP55M7BF0X35T6XMQ'
@@ -14,7 +15,7 @@ path_to_dir = 'clubs'
 db_name = 'db_twitts.db'
 positivfile = 'positive.txt'
 negativfile = 'negative.txt'
-count_twitts = 20
+count_twitts = 4
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -22,84 +23,116 @@ api = tweepy.API(auth)
 
 con = sqlite3.connect(db_name)
 cur = con.cursor()
+print('Started working and connected to the database: ', db_name)
 
 
-def check_for_duplication(user_name, idt):
-    recuestdb = 'SELECT IdTwett FROM ' + user_name
+# function of checking twitts in the table football club
+def check_for_duplication(user_name, club_names, idt):
+    recuestdb = 'SELECT alias, idtwitt FROM ' + club_names
     cur.execute(recuestdb)
     data_column = cur.fetchall()
-    new_data = []
-    for element in data_column:
-        new_data.append(element[0])
-    data_column = new_data
-    del(new_data)
-    for i in data_column:
-        if i == idt:
-            return True
-
-
-def positiv_words(user_name, texttwiit):
-    text_twit_list = texttwiit.split()
-    p = Path(__file__).parents[0]
-    open_positive_words = open(str(p) + '\\' + positivfile, 'r')
-    positiv_word_list = list(map(lambda x:x.strip(),open_positive_words))
-    for i in positiv_word_list:
-        for ii in text_twit_list:
-            if i == ii:
+    for i in range(0, len(data_column)):
+        c = 0
+        a = data_column[i][c]
+        if a == user_name:
+            c = 1
+            b = data_column[i][c]
+            if b == idt:
                 return True
 
 
-def negativ_words(user_name, texttwiit):
-    text_twit_list = texttwiit.split()
+# function search positive word in twitt
+def positiv_words(user_name, club_names, texttwiit):
+    p = Path(__file__).parents[0]
+    open_positive_words = open(str(p) + '\\' + positivfile, 'r')
+    positiv_word_list = list(map(lambda x: x.strip(), open_positive_words))
+    for i in positiv_word_list:
+        res = findall(i, texttwiit)
+        if ''.join(res) != '':
+            return True
+
+
+# function search negative word in twitt
+def negativ_words(user_name, club_names, texttwiit):
     p = Path(__file__).parents[0]
     open_negativ_words = open(str(p) + '\\' + negativfile, 'r')
     negativ_word_list = list(map(lambda x: x.strip(), open_negativ_words))
     for i in negativ_word_list:
-        for ii in text_twit_list:
-            if i == ii:
-                return True
+        res = findall(i, texttwiit)
+        if ''.join(res) != '':
+            return True
 
 
-def twitts_user(user_name, count_twitts):
+# The main function of checking the user's tweets for positive and negative words
+def brain_bot(user_name, club_names, count_twitts):
     public_tweets = api.user_timeline(user_name, count=count_twitts)
+    print('Get the', count_twitts, 'latest tweets user: ', user_name)
     for tweet in public_tweets:
         timetwitt = tweet.created_at
         idt = tweet.id
         texttwiit = tweet.text
-        if check_for_duplication(user_name, idt) == True:
+        if check_for_duplication(user_name, club_names, idt) == True:
             continue
-        elif positiv_words(user_name, texttwiit) == True:
-            NegativWords = ""
-            PositivWords = "1"
-            caunt_positiv = [timetwitt, idt, PositivWords, NegativWords, texttwiit]
-            cur.execute('INSERT INTO %s VALUES(?, ?, ?, ?, ?)' % name_short, caunt_positiv)
+        elif positiv_words(user_name, club_names, texttwiit) == True:
+            NegativWords = 0
+            PositivWords = 1
+            last_parse_day = datetime.datetime.today().strftime("%d%m%Y")
+            caunt_positiv = [user_name, club_names, timetwitt, PositivWords, NegativWords, idt, last_parse_day]
+            cur.execute('INSERT INTO %s VALUES(?, ?, ?, ?, ?, ?, ?)' % club_names, caunt_positiv)
             con.commit()
-        elif negativ_words(user_name, texttwiit) == True:
-            NegativWords = "1"
-            PositivWords = ""
-            caunt_negativ = [timetwitt, idt, PositivWords, NegativWords, texttwiit]
-            cur.execute('INSERT INTO %s VALUES(?, ?, ?, ?, ?)' % name_short, caunt_negativ)
+            print('MADE A POSITIVE TWITT in the table of the club: ', club_names)
+        elif negativ_words(user_name, club_names, texttwiit) == True:
+            NegativWords = 1
+            PositivWords = 0
+            last_parse_day = datetime.datetime.today().strftime("%d%m%Y")
+            caunt_positiv = [user_name, club_names, timetwitt, PositivWords, NegativWords, idt, last_parse_day]
+            cur.execute('INSERT INTO %s VALUES(?, ?, ?, ?, ?, ?, ?)' % club_names, caunt_positiv)
             con.commit()
+            print('MADE A NEGATIVE TWITT in the table of the club: ', club_names)
         else:
             continue
 
 
-while True:
-    cur.execute('CREATE TABLE IF NOT EXISTS DataParsing(UpdataTime TEXT)')
-    updata_time = ctime()
-    cur.execute('INSERT INTO DataParsing VALUES(?)', (updata_time,))
-    con.commit()
-    chdir(path_to_dir)
-    for file in glob("*.txt"):
-        file_open = open(file)
-        for line in file_open:
-            name_short = line.split('/')
-            name_short = name_short[-1]
-            cur.execute('CREATE TABLE IF NOT EXISTS %s'
-                        '(TimeTwitt TEXT, IdTwett INTEGER, '
-                        'PositivWords TEXT, NegativWords TEXT, '
-                        'TextTwitt TEXT)' % name_short)
-            twitts_user(name_short, count_twitts)
-    p = Path(__file__).parents[0]
-    chdir(p)
-    sleep(3600)#falls asleep for 60 minutes and repeats the cycle
+# Create total a table with columns:
+cur.execute('CREATE TABLE IF NOT EXISTS Total_Table('
+            'alias TEXT, '  # alias - name of the twitter user
+            'club_alias TEXT, '  # club_alias - belonging to a football club
+            'lastparse INTEGER)'  # lastparse - recent processing time from the beginning of the era
+            )
+print('Connected to table Total_Table. Columns: alias TEXT, club_alias TEXT, lastparse INTEGER')
+chdir(path_to_dir)
+print('Opened folder: ', path_to_dir)
+for file in glob("*.txt"):
+    print('Find file: ', file)
+    club_names = file.split('.txt')
+    club_names = club_names[0]
+    # Create a football club table with columns:
+    cur.execute('CREATE TABLE IF NOT EXISTS %s('
+                'alias TEXT, '  # alias - name of the twitter user
+                'club_alias TEXT, '  # club_alias - belonging to a football club
+                'time_twitt INTEGER, '  # time_twitt - date of publication of the tweet
+                'positive INTEGER, '  # positive - a sign that the news is positive
+                'negative INTEGER, '  # negative - a sign that the news is negative
+                'idtwitt INTEGER, '  # idtwiit - tweet identification number
+                'lastparseday INTEGER)'  # lastparseday - day of adding to the database
+                % club_names)
+    print('Connected to table football club: ', club_names)
+    file_open = open(file)
+    print('Opened file: ', file)
+    for line in file_open:
+        name_short = line.split('/')
+        name_short = list(map(lambda x: x.strip(), name_short))
+        name_short = name_short[-1]
+        print('Get the name of a twitter-user: ', name_short)
+        last_parse = int(time.time())
+        caunt_total = [name_short, club_names, last_parse]
+        cur.execute('INSERT INTO Total_Table VALUES(?, ?, ?)', caunt_total)
+        con.commit()
+        print('Record in table Total_Table about time parsing. User: ', name_short)
+        print('Check for positive and negative words of the user: ', name_short)
+        brain_bot(name_short, club_names, count_twitts)
+p = Path(__file__).parents[0]
+chdir(p)
+cur.close()
+con.close()
+print('Finished work and disconnected from the database: ', db_name)
